fdb880f7f1a2c3edc72c24db071006cecc469dc8
mdsamikhan0143-rgb <mdsamikhan0143@gmail.com>
2026-08-30 01:52:02 +0530

    Time: 0 ms (100%), Space: 33.3 MB (81.14%) - LeetHub

---
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> limitOccurrences(vector<int>& nums, int k) {
+        vector<int> ans;
+
+        for (int x : nums) {
+            if (ans.empty() || ans.back() != x) {
+                ans.push_back(x);
+            }
+            else {
+                if (count(ans.begin(), ans.end(), x) < k) {
+                    ans.push_back(x);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
