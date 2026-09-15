05be3f70344f5f6ddca98e7655b08837230b7cd2
Md Sami <mdsamikhan0143@gmail.com>
2026-09-15 17:53:25 +0530

    Time: 16 ms (19.57%), Space: 35.1 MB (54.42%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,41 +1,34 @@
-class Solution {
-public:
-    // Your code: calculate days needed for a capacity
-    int requiredDays(vector<int>& weights, int capacity) {
-        int days = 1;
-        int currentLoad = 0;
+class Solution{
+    public:
+     int daysNeeded(vector<int>& weights, int capacity) {
+        int days = 1 ;
+        int currnentload = 0;
+     for(int w : weights){
+         if(currnentload + w > capacity){
+            days ++;
+            currnentload = w ;
+         }else{
+            currnentload += w ;
+         }
+     }
+     return days;
+  }
 
-        for (int w : weights) {
-            if (currentLoad + w > capacity) {
-                days++;
-                currentLoad = w;
-            } else {
-                currentLoad += w;
-            }
+  int shipWithinDays(vector<int>& weights, int days) {
+     int low = *max_element(weights.begin(),weights.end());
+     int high =  0;
+     for( int w : weights){
+        high += w ;
+     }
+     while(low<=high){
+        int mid = (  high + low) / 2 ;
+
+        if(daysNeeded(weights,mid) <= days){
+                high = mid -1 ;
+        }else{
+            low= mid +1;
         }
-
-        return days;
-    }
-
-    // LeetCode calls this function
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = 0;
-
-        for (int w : weights) {
-            high += w;
-        }
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (requiredDays(weights, mid) <= days) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
-    }
+     }
+     return low ;
+  }
 };
