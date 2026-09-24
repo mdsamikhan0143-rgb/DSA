c76b02d9beec8980eb67f4449080d1008c498e43
Md Sami <mdsamikhan0143@gmail.com>
2026-09-24 20:17:21 +0530

    Time: 3 ms (2.06%), Space: 15.8 MB (54.05%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;
-
-        // Find the rightmost element smaller than its next element.
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        if (i >= 0) {
-            int j = n - 1;
-
-            // Find the rightmost element greater than nums[i].
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-
-            swap(nums[i], nums[j]);
-        }
-
-        // Arrange the remaining elements in the smallest order.
-        reverse(nums.begin() + i + 1, nums.end());
+        std::next_permutation(nums.begin(), nums.end());
     }
 };
