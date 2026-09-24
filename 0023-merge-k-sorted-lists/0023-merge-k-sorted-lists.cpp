f0617ae7a949733617dc9bd4028b63d452ca2d92
Md Sami <mdsamikhan0143@gmail.com>
2026-09-24 20:19:20 +0530

    Time: 0 ms (100%), Space: 19 MB (24.19%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    ListNode* mergeTwo(ListNode* a, ListNode* b) {
+        ListNode dummy;
+        ListNode* tail = &dummy;
+
+        while (a != nullptr && b != nullptr) {
+            if (a->val <= b->val) {
+                tail->next = a;
+                a = a->next;
+            } else {
+                tail->next = b;
+                b = b->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = (a != nullptr) ? a : b;
+        return dummy.next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+
+        if (k == 0) return nullptr;
+
+        while (k > 1) {
+            int write = 0;
+
+            for (int i = 0; i < k; i += 2) {
+                ListNode* a = lists[i];
+                ListNode* b = (i + 1 < k) ? lists[i + 1] : nullptr;
+
+                lists[write++] = mergeTwo(a, b);
+            }
+
+            k = write;
+        }
+
+        return lists[0];
+    }
+};
