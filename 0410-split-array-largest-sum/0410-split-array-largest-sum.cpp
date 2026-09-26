7d1159ebc379d542b09e4202036e9d3fbec9d5dc
Md Sami <mdsamikhan0143@gmail.com>
2026-09-26 13:25:43 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.2%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    int countSubarrays(vector<int>& nums, long long limit) {
-        int subarrays = 1;
+    int countsubarrys(vector<int>& nums, int k){
+        int subarrys = 1 ;
         long long currentSum = 0;
-
-        for (int num : nums) {
-            if (currentSum + num > limit) {
-                subarrays++;
-                currentSum = num;
-            } else {
-                currentSum += num;
-            }
-        }
-
-        return subarrays;
+    for(int num : nums){
+        if (currentSum + num > k){
+            subarrys++;
+            currentSum = num; 
+        }else{
+            currentSum += num;
+      }
     }
+    return subarrys;
+  }
 
-    int splitArray(vector<int>& nums, int k) {
-        long long low = *max_element(nums.begin(), nums.end());
-        long long high = 0;
+  int splitArray(vector<int>& nums, int k){
+    int low = *max_element(nums.begin(), nums.end());
+    int  high = 0 ;
 
-        for (int num : nums) {
-            high += num;
-        }
-
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;
-
-            if (countSubarrays(nums, mid) <= k) {
-                high = mid - 1;  // Try a smaller maximum sum
-            } else {
-                low = mid + 1;   // Need a larger maximum sum
-            }
-        }
-
-        return static_cast<int>(low);
+    for(int num : nums ){
+         high += num;
     }
+    while (low <= high){
+        long  long mid = (high + low)/2;
+       if (countsubarrys( nums, mid)<= k){
+            high = mid -1 ;
+       }
+       else {
+            low = mid  +1 ;
+       }
+    }
+    return low ;
+  }
 };
